042f9840e6a9881b0e89b9421f2f66207bde91e1
hukurou <junjun.0712.on@gmail.com>
2023-07-24 17:56:00 +0900

    add funcs_copy(function to use in GUI)

---
+++ funcs_copy.py
@@ -0,0 +1,74 @@
+import openai
+import secret
+
+openai.api_key = secret.api_key
+
+def upload_file_train(path):
+    path = path.replace('"', '')
+    # ファイルアップロード（学習）
+    upload_file_train = openai.File.create(
+                                        file=open(path, "rb"), # ファイル（JSONL）
+                                        purpose='fine-tune',             # ファイルのアップロード目的
+                                                )
+
+    # 出力
+    print(upload_file_train)
+    path = 'history.txt'
+    with open(path, mode='a',encoding='utf-8') as f:
+        f.write("学習用データ\n")
+        f.write("train_data=")
+        f.write(path)
+        f.write("\n")
+        f.write(str(upload_file_train))
+        f.write("\n\n")
+
+def upload_file_val(path):
+    path = path.replace('"', '')
+    # ファイルアップロード（学習）
+    upload_file_train = openai.File.create(
+                                        file=open(path, "rb"), # ファイル（JSONL）
+                                        purpose='fine-tune',             # ファイルのアップロード目的
+                                                )
+
+    # 出力
+    print(upload_file_train)
+    path = 'history.txt'
+    with open(path, mode='a',encoding='utf-8') as f:
+        f.write("検証用データ\n")
+        f.write("val_data=")
+        f.write(path)
+        f.write("\n")
+        f.write(str(upload_file_train))
+        f.write("\n\n")
+
+def fine_train_val(training_file_id, validation_file_id, model = 'davinci'):
+    FineTune = openai.FineTune.create(training_file   = training_file_id,    
+                                    validation_file = validation_file_id,          
+                                    model           = model,                  
+                                    ) 
+    print(FineTune.id)
+    path = 'history.txt'
+    with open(path, mode='a',encoding='utf-8') as f:
+        f.write("finetuning\n")
+        f.write("training_file_id = ")
+        f.write(training_file_id)
+        f.write("\n")
+        f.write("validation_file_id = ")
+        f.write(validation_file_id)
+        print("\n")
+        f.write(f"FineTune_id = {str(FineTune.id)}")
+        f.write("\n\n")
+
+def fine_train(file_id, model = 'davinci'):
+    FineTune = openai.FineTune.create(training_file   = file_id,     
+                                    model           = model,       
+                                    ) 
+    print(FineTune.id)
+    path = 'history.txt'
+    with open(path, mode='a',encoding='utf-8') as f:
+        f.write("finetuning\n")
+        f.write("training_file_id = ")
+        f.write(file_id)
+        f.write("\n")
+        f.write(f"FineTune_id = {str(FineTune.id)}")
+        f.write("\n\n")
